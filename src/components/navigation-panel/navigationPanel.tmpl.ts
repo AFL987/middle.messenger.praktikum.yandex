988d9229fc7e0d944627a7e48d5df61cf6796b79
Aleksander <afedorenko@mail.uma.tech>
2022-03-20 22:35:33 +0300

    fix navigation-panel

--- src/components/navigation-panel/navigationPanel.tmpl.ts
+++ src/components/navigation-panel/navigationPanel.tmpl.ts
@@ -1,17 +1,10 @@
 export default `
 .navigation-panel Временная панель <br/> навигации
-	div
-		a(href= '/') главная
-	div
-		a(href= '/signup') регистрация
-	div
-		a(href= '/chats') чаты
-	div
-		a(href= '/profile') профиль
-	div
-		a(href= '/password-change') смена пароля	
-	div
-		a(href= '/404') 404
-	div
-		a(href= '/500') 500				
+	#link1
+	#link2
+	#link3
+	#link4
+	#link5
+	#link6
+	#link7
 `;

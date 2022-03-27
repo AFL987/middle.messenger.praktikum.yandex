bc88e3a94972663cec78d2f6df59ef3879193a3e
Aleksander <afedorenko@mail.uma.tech>
2022-03-27 08:57:40 +0300

    fix backPane route

--- src/components/back-panel/backPanel.tmpl.ts
+++ src/components/back-panel/backPanel.tmpl.ts
@@ -1,6 +1,4 @@
-import svg from '../../../static/img/*.svg';
 export default `
 .back-panel
-	a(href=back)
-		img(src="${svg.arrou_blue}")
+	#profileIcon
 `;
--- src/components/back-panel/backPanel.ts
+++ src/components/back-panel/backPanel.ts
@@ -1,6 +1,8 @@
 import {render} from 'pug';
 import Block from '../../utils/block';
 import template from './backPanel.tmpl';
+import IconLink from "../iconLink";
+import arrowBlue from '../../../static/img/arrou_blue.svg';
 
 type Props = {
 	back: string;
@@ -8,7 +10,15 @@ type Props = {
 
 export default class BackPanel extends Block {
     constructor(props: Props) {
-			super({...props});
+
+			const profileIcon = new IconLink({
+				className: 'profile',
+				srcIcon: arrowBlue,
+				link: props.back,
+			});
+
+
+			super({children: {profileIcon}});
     }
     render(): string {
 				const {back} = this.props;
